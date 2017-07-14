6811afaa9b50ed59ac465e6c889256d1e69a949a
Sven Waweru <svenwaweru@Svens-MacBook-Pro.local>
2017-07-14 09:39:15 -0400

    Minor updates.

--- editjson.sh
+++ editjson.sh
@@ -1,7 +1,6 @@
-sudo su
 curl -SLO "https://s3.amazonaws.com/codeship-jet-releases/1.18.0/jet-linux_amd64_1.18.0.tar.gz"
-tar -xaC /usr/local/bin -f jet-linux_amd64_1.18.0.tar.gz
-chmod +x /usr/local/bin/jet
+sudo tar -xaC /usr/local/bin -f jet-linux_amd64_1.18.0.tar.gz
+sudo chmod +x /usr/local/bin/jet
 jet decrypt jsonvars.env.encrypted jsonvars.env.encrypted
 python editjson.py
 jet encrypt jsonvars.env.encrypted jsonvars.env.encrypted
