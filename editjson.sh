114170d3ee51daa4ed69c586e01dc96687115d52
Sven Waweru <svenwaweru@Svens-MacBook-Pro.local>
2017-07-19 17:07:55 -0400

    Testing injection of vars into json file.

--- editjson.sh
+++ editjson.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-apt-get install sudo
-apt-get update
-curl -SLO "https://s3.amazonaws.com/codeship-jet-releases/1.18.0/jet-linux_amd64_1.18.0.tar.gz"
-sudo tar -xaC /usr/local/bin -f jet-linux_amd64_1.18.0.tar.gz
-sudo chmod +x /usr/local/bin/jet
 jet decrypt jsonvars.env.encrypted jsonvars.env.encrypted
 python editjson.py
 jet encrypt jsonvars.env.encrypted jsonvars.env.encrypted
